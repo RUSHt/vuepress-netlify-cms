641618a08e9b0d37001c3a04c18b26f35ae8237a
RUSHt <petera@rush.technology>
2022-02-02 20:54:59 +0000

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Netlify CMS + VuePress',
+  description: 'Netlify + VuePress',
+  themeConfig: {
+    docsDir: 'docs',
+    repo: 'RUSHt/vuepress-netlify-cms',
+    sidebar: [
+      '/',
+      '/welcome',
+      '/test'
+    ],
+    nav: [
+      {
+        text: 'Admin',
+        link: '/admin/#/',
+      }
+    ]
+  }
+}
